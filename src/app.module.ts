bbdcee01023e072790bf73948738a5f1f69f4c38
Thitiphong Nimmak <64160048@go.buu.ac.th>
2024-09-02 01:41:10 +0700

    setting controller math

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ControllerModule } from './math/controller/controller.module';
 import { MathController } from './math/math.controller';
 import { MathService } from './math/math.service';
 
 @Module({
-  imports: [ControllerModule],
+  imports: [],
   controllers: [AppController, MathController],
   providers: [AppService, MathService],
 })
