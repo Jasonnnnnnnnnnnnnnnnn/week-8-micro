e487bc80fc709d22de9f939aa3403262872b2896
Thitiphong Nimmak <64160048@go.buu.ac.th>
2024-09-04 03:58:06 +0700

    finished

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(3001);
+
+  const microserApp = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '127.0.0.1',
+        port: 8877,
+
+      },
+    },
+  );
+  await microserApp.listen();
 }
 bootstrap();
--- src/math/math.controller.ts
+++ src/math/math.controller.ts
@@ -1,14 +1,23 @@
 import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { MathService } from './math.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller('math')
 export class MathController {
-    private logger = new Logger('MathController');
+  private logger = new Logger('MathController');
 
-    constructor(private readonly mathservice: MathService) {}
-    @Post('sum')
-    sum(@Body('numbers') numbers: number[]): number {
-        this.logger.log(' sum ' + numbers);
-        return this.mathservice.accumulate(numbers);
-      }
+  constructor(private readonly mathservice: MathService) { }
+
+  @Post('sum')
+  sum1(@Body('numbers') numbers: number[]): number {
+    this.logger.log(' sum ' + numbers);
+    return this.mathservice.accumulate(numbers);
+  }
+  @MessagePattern({ cmd: 'sum' })
+  sum2(numbers: number[]): number {
+    this.logger.log(' sum2 ' + numbers);
+    return this.mathservice.accumulate(numbers);
+  }
+
+
 }
